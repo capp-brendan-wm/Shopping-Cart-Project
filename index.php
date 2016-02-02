e4f7d537543cd602f8ad6a1c4318745597b1dd67
Brendan Capp <brendan.capp@west-mec.org>
2016-02-02 13:39:44 -0700

    This is the current progress my shopping cart project has made.

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
+<!-- OPEN IN FIREFOX IF THE VIDEO DOES NOT LOAD RIGHT AWAY --- DO NOT OPEN IN SAFARI @ ALL --- IF YOU OPEN IN CHROME IT MIGHT TAKE SOME TIME FOR THE VIDEO TO LOAD -->
+<?php
 
+/*** mysql hostname ***/
+$hostname = '127.0.0.1';
+
+/*** mysql username ***/
+$username = 'root';
+
+/*** mysql password ***/
+$password = 'root';
+
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=rentapet", $username, $password);
+    /*** echo a message saying we have connected ***/
+    echo 'Connected to database';
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+}
+
+$sql = "select * from animals";
+
+foreach($dbh->query($sql) as $row){
+    echo $row['name'];
+}
+
+?>
 
 
 <!DOCTYPE html>
@@ -7,11 +35,16 @@
     <script src="../RentAPet/jquery.js"></script>
     <title>The Bread Cart</title>
 </head>
+
 <body>
-<div>
 
+<div id="navbar">
 
-
+    <table id="navt">
+        <tr id="navtr">
+            <div id="home"><td><a href="index.php">Home</a></td></div>
+        </tr>
+    </table>
 
 </div>
 <div>
---
+++ search.php
@@ -0,0 +1,84 @@
+<?php
+
+/*** mysql hostname ***/
+$hostname = '127.0.0.1';
+
+/*** mysql username ***/
+$username = 'root';
+
+/*** mysql password ***/
+$password = 'root';
+
+try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=breadcart", $username, $password);
+    /*** echo a message saying we have connected ***/
+}
+catch(PDOException $e)
+{
+}
+//animals=table name
+$stmt = $dbh->prepare("SELECT * FROM animals a WHERE a.type = :animal");
+$stmt->execute(array(':animal'=>$_GET['animal']));
+$results = $stmt->fetchAll();
+
+?>
+<!DOCTYPE html>
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link rel="icon" type="image/png" href="http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/black-paint-splatter-icons-alphanumeric/069096-black-paint-splatter-icon-alphanumeric-letter-rr.png">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <script src="jquery.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    <title>RentAPet</title>
+</head>
+
+<body>
+<div id="nav">
+    <div id="rentapet">
+        <p>Rent-A-Pet - Search</p>
+    </div>
+    <table id="nava">
+
+        <tr>
+            <div id="home"><td><a href="index.php">Home</a></td></div>
+            <div id="bag"><td><a href="payment.php"><img src="Pictures/bagz.png" ></a></td></div>
+            <div id="SignIn"><td><a href="signin.php">Sign In</a></td></div>
+        </tr>
+    </table>
+</div>
+
+<div id="searchinfo">
+<h1><?php echo $_GET['animal'];?></h1>
+
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        if(count($results) > 0) {
+            foreach($results as $animal){
+
+                $animalname = $animal['name'];
+
+                echo '<tr>';
+                echo "<td><a href='animalProfile.php?id=" . $animal['id'] . "'>{$animalname}</a></td>";
+                echo '</tr>';
+            }
+        }
+        else{
+            echo '<tr>';
+            echo '<td>0 Results Found.</td>';
+            echo '</tr>';
+        }
+        ?>
+        </tbody>
+    </table>
+    <?php
+
+?>
+</div>
+</body>
+
